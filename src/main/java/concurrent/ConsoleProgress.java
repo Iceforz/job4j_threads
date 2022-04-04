3225b4a1133e02c292057d6db0a0454455b67905
iceforz <iceforz3@gmail.com>
2022-04-04 13:09:27 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package concurrent;
+import java.lang.Runnable;
+
+public class ConsoleProgress implements Runnable{
+
+    @Override
+
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+                System.out.print("\r Loading...|." + "\\");
+                Thread.sleep(500);
+                System.out.print("\r Loading...|." + "|");
+                Thread.sleep(500);
+                System.out.print("\r Loading...|." + "/");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1550);
+        progress.interrupt();
+    }
+}
---
+++ src/main/java/concurrent/Runnable.java
@@ -0,0 +1,5 @@
+package concurrent;
+
+ interface Runnable {
+    public void run();
+}
