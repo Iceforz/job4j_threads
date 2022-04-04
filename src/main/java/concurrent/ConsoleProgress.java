55ff22c84b051d4ed2b1e0797503f2bb350059b2
iceforz <iceforz3@gmail.com>
2022-04-04 16:01:45 +0300

    3. Прерывание нити [#1019 #284206]

--- src/main/java/concurrent/ConsoleProgress.java
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -6,20 +6,21 @@ public class ConsoleProgress implements Runnable{
     @Override
 
     public void run() {
+        String[] chars = {"\\", "|", "/"};
+        int index = 0;
         while (!Thread.currentThread().isInterrupted()) {
-            try {
-                Thread.sleep(500);
-                System.out.print("\r Loading...|." + "\\");
-                Thread.sleep(500);
-                System.out.print("\r Loading...|." + "|");
-                Thread.sleep(500);
-                System.out.print("\r Loading...|." + "/");
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+            if (index == 3) {
+                index = 0;
             }
-        }
+                try {
+                    Thread.sleep(500);
+                    System.out.print("\r Loading...| " + chars[index++]);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
     }
-
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
