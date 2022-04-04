b2a1cdc6c67f9212a3aaee55ab2391f5547266e8
iceforz <iceforz3@gmail.com>
2022-04-04 20:49:26 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/concurrent/Wget.java
+++ src/main/java/concurrent/Wget.java
@@ -1,22 +1,53 @@
 package concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        int index = 0;
-                        for (;index <= 100; index++) {
-                            Thread.sleep(1000);
-                            System.out.print("\rLoading : " + index + "%");
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.Runnable;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final String fileName;
+    private final int speed;
+
+    public Wget(String url, String fileName, int speed) {
+        this.url = url;
+        this.fileName = fileName;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(this.url).openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+                byte[] dataBuffer = new byte[1024];
+                int bytesRead;
+                long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0 ,1024)) !=-1) {
+                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+                    if (bytesRead >= speed) {
+                        long finish = System.currentTimeMillis();
+                        long time = finish - start;
+                        if (time < 1000) {
+                            Thread.sleep(1000 - time);
+                            start = System.currentTimeMillis();
                         }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
                     }
                 }
-        );
-        thread.start();
-        System.out.println("Main");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        String filename = args[1];
+        int speed = Integer.parseInt(args[2]);
+        Thread wget = new Thread(new Wget(url, filename, speed));
+        wget.start();
+        wget.join();
     }
 }
-
