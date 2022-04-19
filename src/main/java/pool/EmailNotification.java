4c7bc7f9664aa5fd51407dc2afff8a9e02c9cb88
iceforz <iceforz3@gmail.com>
2022-04-19 15:13:59 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/pool/EmailNotification.java
@@ -0,0 +1,36 @@
+package pool;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final List<Thread> threads = new LinkedList<>();
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+
+   public void emailTo(User user) throws InterruptedException {
+       pool.submit(() -> {
+           String subject = String.format("Notification {%s} to email {%s}.",
+                   user.getUsername(), user.getEmail());
+           String body = String.format("Add a new event to {%s}", user.getUsername());
+           send(subject, body, user.getEmail());
+       });
+   }
+
+   public void close() {
+       pool.shutdown();
+       while (!pool.isTerminated()) {
+           try {
+               Thread.sleep(100);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }
+   }
+    public void send(String subject, String body, String email) {
+  }
+}
